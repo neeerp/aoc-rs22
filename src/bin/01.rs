ca68f8ff524ccb670db958a65150b4a28851f05f
David Goldstein <goldstein.g.david@gmail.com>
2023-07-22 18:08:27 -0400

    Solve day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,5 +1,14 @@
+/// Find the elf carrying the most calories; how much are they carrying?
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let elves = input.trim_end().split("\n\n");
+    elves.map(sum_elf).max()
+}
+
+pub fn sum_elf(input: &str) -> u32 {
+    input
+        .split('\n')
+        .map(|n| -> u32 { n.parse::<u32>().unwrap() })
+        .sum()
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -17,9 +26,15 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_sum_elf() {
+        let input = "1\n2\n3";
+        assert_eq!(sum_elf(&input), 6);
+    }
+
+    #[test]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 1);
-        assert_eq!(part_one(&input), None);
+        assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
