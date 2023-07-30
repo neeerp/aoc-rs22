380cee749bc211acbc921fd8141aa7435111c23d
David Goldstein <goldstein.g.david@gmail.com>
2023-07-30 14:12:15 -0400

    Solve day 4 part 2

--- src/bin/04.rs
+++ src/bin/04.rs
@@ -14,7 +14,7 @@ pub fn range_contains(first: &str, second: &str) -> bool {
     let (l1, e1) = parse_range(first);
     let (l2, e2) = parse_range(second);
 
-    return (l1 <= l2 && e1 >= e2) || (l2 <= l1 && e2 >= e1);
+    (l1 <= l2 && e1 >= e2) || (l2 <= l1 && e2 >= e1)
 }
 
 /// Parses a range of the form i-j into two boundaries (i, j)
@@ -31,8 +31,30 @@ pub fn parse_pairs(pairs: &str) -> (&str, &str) {
     (split.clone().nth(0).unwrap(), split.clone().nth(1).unwrap())
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+/// How many pairs overlap at all?
+pub fn part_two(input: &str) -> Option<usize> {
+    Some(input.trim_end().split('\n').map(is_overlapping).sum())
+}
+
+pub fn is_overlapping(pairs: &str) -> usize {
+    let (left, right) = parse_pairs(pairs);
+    range_overlaps(left, right) as usize
+}
+
+pub fn range_overlaps(first: &str, second: &str) -> bool {
+    let (l1, e1) = parse_range(first);
+    let (l2, e2) = parse_range(second);
+
+    // Boundaries touch
+    if l1 == l2 || e1 == e2 || l1 == e2 || l2 == e1 {
+        return true;
+    }
+
+    if l1 < l2 {
+        e1 > l2
+    } else {
+        e2 > l1
+    }
 }
 
 fn main() {
@@ -46,18 +68,36 @@ mod tests {
     use super::*;
 
     #[test]
+    fn test_range_does_not_overlap() {
+        assert!(!range_overlaps("1-4", "5-6"));
+        assert!(!range_overlaps("5-6", "1-4"));
+    }
+
+    #[test]
+    fn test_range_overlaps() {
+        assert!(range_overlaps("1-5", "5-6"));
+        assert!(range_overlaps("5-6", "1-5"));
+    }
+
+    #[test]
+    fn test_contained_range_overlaps() {
+        assert!(range_overlaps("1-5", "2-5"));
+        assert!(range_overlaps("2-5", "1-5"));
+    }
+
+    #[test]
     fn test_range_contains() {
-        assert!(range_contains("1-5", "2-5"))
+        assert!(range_contains("1-5", "2-5"));
     }
 
     #[test]
     fn test_range_does_not_contain() {
-        assert!(!range_contains("1-5", "2-6"))
+        assert!(!range_contains("1-5", "2-6"));
     }
 
     #[test]
     fn test_parse_range() {
-        assert_eq!(parse_range("1-5"), (1, 5))
+        assert_eq!(parse_range("1-5"), (1, 5));
     }
 
     #[test]
@@ -69,6 +109,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let input = advent_of_code::read_file("examples", 4);
-        assert_eq!(part_two(&input), None);
+        assert_eq!(part_two(&input), Some(4));
     }
 }
