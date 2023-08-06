54a296fb5614ffb03c8bc1979fc9b4999efe1578
David Goldstein <goldstein.g.david@gmail.com>
2023-08-06 16:08:13 -0400

    Solve day 6 part 1

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,5 +1,21 @@
-pub fn part_one(_input: &str) -> Option<u32> {
-    None
+use itertools::Itertools;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let (offset, _) = input
+        .chars()
+        .tuple_windows::<(char, char, char, char)>()
+        .fold((4, false), |(offset, found), (a, b, c, d)| {
+            if found {
+                return (offset, found);
+            }
+
+            if [a, b, c, d].into_iter().all_unique() {
+                (offset, true)
+            } else {
+                (offset + 1, false)
+            }
+        });
+    Some(offset)
 }
 
 pub fn part_two(_input: &str) -> Option<u32> {
@@ -17,7 +33,6 @@ mod tests {
     use super::*;
 
     #[test]
-    #[ignore]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 6);
         assert_eq!(part_one(&input), Some(7));
