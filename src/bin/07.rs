d03eaf758366b271c547906e2f95547fe51dc469
David Goldstein <goldstein.g.david@gmail.com>
2023-08-07 19:31:44 -0400

    Solve day 7 part 1

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,13 +1,73 @@
-use std::path::PathBuf;
+use std::{cell::RefCell, collections::HashMap, iter::Peekable, path::PathBuf, rc::Rc, str::Split};
 
 use regex::Regex;
 
 pub fn part_one(input: &str) -> Option<u32> {
-    // Strategy might go something like this:
-    //
-    // Track where we currently are (cd commands)
-    //
-    None
+    let all_dirs = execute(input.trim_end());
+
+    let mut root = all_dirs.get(&PathBuf::from("/")).unwrap().borrow_mut();
+    let mut results = root.resolve(&all_dirs);
+    results.push(root.size);
+
+    Some(results.iter().filter(|s| **s < 100_000).sum::<u32>())
+}
+
+pub fn execute(input: &str) -> HashMap<PathBuf, Rc<RefCell<Directory>>> {
+    let mut execution = input.split('\n').peekable();
+    let mut cwd = PathBuf::new();
+
+    let mut all_dirs: HashMap<PathBuf, Rc<RefCell<Directory>>> = HashMap::new();
+
+    while let Some(line) = execution.next() {
+        let cmd = parse_command(line);
+        match cmd {
+            Command::LS => {
+                execute_ls(&mut all_dirs, &mut execution, cwd.clone());
+            }
+            Command::CD(dir) => {
+                cwd = execute_cd(dir, cwd.clone());
+            }
+        };
+    }
+
+    all_dirs
+}
+
+pub fn execute_ls(
+    all_dirs: &mut HashMap<PathBuf, Rc<RefCell<Directory>>>,
+    execution: &mut Peekable<Split<'_, char>>,
+    cwd: PathBuf,
+) {
+    if let None = all_dirs.get(&cwd) {
+        all_dirs.insert(
+            cwd.clone(),
+            Rc::new(RefCell::new(Directory::new(cwd.clone()))),
+        );
+    }
+
+    let mut cur = all_dirs.get_mut(&cwd).unwrap().borrow_mut();
+
+    while let Some(next_line) = execution.peek() {
+        // Bail if we've reached the end of the ls output
+        if is_command(*next_line) {
+            break;
+        }
+        let line = execution.next().unwrap();
+
+        // Parse ls output line and update the current directory
+        let ls_output = parse_ls_output(line);
+        match ls_output {
+            LsOutput::Dir(dir) => {
+                let mut child_path = cwd.clone();
+                child_path.push(dir);
+                cur.children.push(child_path);
+            }
+            LsOutput::File(_, size) => {
+                cur.size += size;
+                cur.file_sizes.push(size)
+            }
+        }
+    }
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -18,6 +78,40 @@ pub fn part_two(input: &str) -> Option<u32> {
 pub enum Command<'a> {
     LS,
     CD(&'a str),
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Directory {
+    pub path: PathBuf,
+    pub size: u32,
+    pub children: Vec<PathBuf>,
+    pub file_sizes: Vec<u32>,
+}
+
+impl Directory {
+    pub fn new(path: PathBuf) -> Self {
+        Directory {
+            path,
+            size: 0,
+            children: Vec::new(),
+            file_sizes: Vec::new(),
+        }
+    }
+
+    /// Resolve the size by resolving all children.
+    pub fn resolve(&mut self, all_dirs: &HashMap<PathBuf, Rc<RefCell<Directory>>>) -> Vec<u32> {
+        let mut resolved_sizes = vec![];
+        for child_path in self.children.iter() {
+            let mut child = all_dirs.get(child_path).unwrap().borrow_mut();
+
+            let mut child_sizes = child.resolve(all_dirs);
+            resolved_sizes.append(&mut child_sizes);
+
+            self.size += child.size;
+        }
+        resolved_sizes.push(self.size);
+        resolved_sizes
+    }
 }
 
 pub fn parse_command(line: &str) -> Command {
@@ -33,23 +127,43 @@ pub fn parse_command(line: &str) -> Command {
     }
 }
 
-pub fn execute_cd<'a>(cmd: Command, mut pwd: PathBuf) -> PathBuf {
+pub fn execute_cd<'a>(cmd: &'a str, mut pwd: PathBuf) -> PathBuf {
     match cmd {
-        Command::CD("/") => PathBuf::from("/"),
-        Command::CD("..") => {
+        "/" => PathBuf::from("/"),
+        ".." => {
             pwd.pop();
             pwd
         }
-        Command::CD(dir) => {
+        dir => {
             pwd.push(String::from(dir) + "/");
             pwd
         }
-        Command::LS => panic!("ERROR: called execute_cd with LS command!"),
     }
 }
 
 pub fn is_command(line: &str) -> bool {
     line.chars().next().unwrap() == '$'
+}
+
+pub enum LsOutput<'a> {
+    Dir(&'a str),
+    File(&'a str, u32),
+}
+
+pub fn parse_ls_output<'a>(line: &'a str) -> LsOutput<'a> {
+    let r = Regex::new(r"^(dir (?<dir>.+))|((?<size>\d+) (?<file>.+))$").expect("Oops!");
+    let caps = r.captures(line).expect("Oops!");
+
+    match caps {
+        caps if matches!(caps.name("dir"), Some(_)) => {
+            LsOutput::Dir(caps.name("dir").unwrap().as_str())
+        }
+        caps if matches!(caps.name("file"), Some(_)) => LsOutput::File(
+            caps.name("file").unwrap().as_str(),
+            caps.name("size").unwrap().as_str().parse().unwrap(),
+        ),
+        _ => panic!("Oops!"),
+    }
 }
 
 fn main() {
@@ -68,16 +182,16 @@ mod tests {
     fn test_execute_cd() {
         let mut pwd = PathBuf::new();
 
-        pwd = execute_cd(Command::CD("/"), pwd);
+        pwd = execute_cd("/", pwd);
         assert_eq!(pwd.as_path(), Path::new("/"));
 
-        pwd = execute_cd(Command::CD("foo"), pwd);
+        pwd = execute_cd("foo", pwd);
         assert_eq!(pwd.as_path(), Path::new("/foo/"));
 
-        pwd = execute_cd(Command::CD("bar"), pwd);
+        pwd = execute_cd("bar", pwd);
         assert_eq!(pwd.as_path(), Path::new("/foo/bar/"));
 
-        pwd = execute_cd(Command::CD(".."), pwd);
+        pwd = execute_cd("..", pwd);
         assert_eq!(pwd.as_path(), Path::new("/foo/"));
     }
 
@@ -95,6 +209,7 @@ mod tests {
     #[test]
     fn test_not_is_command() {
         assert!(!is_command("ls"));
+        assert!(!is_command("dir a"));
     }
 
     #[test]
@@ -105,7 +220,7 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
+    // #[ignore]
     fn test_part_one() {
         let input = advent_of_code::read_file("examples", 7);
         assert_eq!(part_one(&input), Some(95_437));
